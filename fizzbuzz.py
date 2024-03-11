85b1ecb5042234a69203b00789b4584dbf946f86
Caio Wirthmann <108591699+caiowirthmann@users.noreply.github.com>
2024-03-10 23:42:23 -0300

    Update fizzbuzz.py
    atualizado com timer para mensurar tempo de execução

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,14 @@
+import time
+
+
 lut = {
     3 : "fizz",
     5 : "buzz"
 }
 
 def fizzbuzz(start, end):
+    start_time = time.time()
+    
     for i in range(start, end):
         result = []
         for nums in lut:
@@ -13,6 +18,9 @@ def fizzbuzz(start, end):
             print ("".join(result))
         else:
             print (i)
+    
+    end_time = time.time()
+    print (f'Tempo de execução = {end_time - start_time}')
     
 
 fizzbuzz(1, 10000)
